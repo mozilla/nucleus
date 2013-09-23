8acbd3a2062b41de9c2940af585107a14bb171e8
Josh Mize <jgmize@gmail.com>
2013-09-23 12:07:53 -0500

    Integrate RNA

--- nucleus/urls.py
+++ nucleus/urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.conf.urls.defaults import patterns, include
+from django.conf.urls.defaults import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from .examples import urls
@@ -7,9 +7,8 @@ from .examples import urls
 from funfactory.monkeypatches import patch
 patch()
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Example:
@@ -21,13 +20,10 @@ urlpatterns = patterns('',
             "User-agent: *\n%s: /" % 'Allow' if settings.ENGAGE_ROBOTS else 'Disallow' ,
             mimetype="text/plain"
         )
-    )
+    ),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^rna', include('rna.urls')),
 )
 
 ## In DEBUG mode, serve media files through Django.
