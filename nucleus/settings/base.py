24857ea0088ab1004345967556d20aa7af2fd4da
Josh Mize <jgmize@gmail.com>
2013-09-25 01:00:30 -0500

    Add REST_FRAMEWORK settings

--- nucleus/settings/base.py
+++ nucleus/settings/base.py
@@ -103,3 +103,20 @@ LOGGING = dict(loggers=dict(playdoh = {'level': logging.DEBUG}))
 
 MIDDLEWARE_CLASSES = get_middleware(
     exclude=['funfactory.middleware.LocaleURLMiddleware'])
+
+REST_FRAMEWORK = {
+    # Use hyperlinked styles by default.
+    # Only used if the `serializer_class` attribute is not set on a view.
+    'DEFAULT_MODEL_SERIALIZER_CLASS':
+        'rest_framework.serializers.HyperlinkedModelSerializer',
+
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ),
+
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
