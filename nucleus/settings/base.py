0d35d4ce36f79b8fc6272595c15c4b7451d8ff1c
Josh Mize <jgmize@gmail.com>
2013-09-26 01:04:44 -0500

    Update to latest RNA and use new filter backend
    Set rna.filters.TimestampedFilterBackend to be the new
    default filter backend.
    
    Add django-filter dependency to vendor-local.
    
    Switched to clients branch

--- nucleus/settings/base.py
+++ nucleus/settings/base.py
@@ -118,5 +118,7 @@ REST_FRAMEWORK = {
 
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-    )
+    ),
+
+    'DEFAULT_FILTER_BACKENDS': ('rna.filters.TimestampedFilterBackend',)
 }
