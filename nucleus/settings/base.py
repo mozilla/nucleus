c6b610daef53ae6b9032d667fae301000bf67df9
Josh Mize <jgmize@gmail.com>
2013-10-22 01:04:45 -0500

    Use HyperlinkedModelSerializerWithPkField
    By default

--- nucleus/settings/base.py
+++ nucleus/settings/base.py
@@ -114,7 +114,7 @@ REST_FRAMEWORK = {
     # Use hyperlinked styles by default.
     # Only used if the `serializer_class` attribute is not set on a view.
     'DEFAULT_MODEL_SERIALIZER_CLASS':
-        'rest_framework.serializers.HyperlinkedModelSerializer',
+        'rna.serializers.HyperlinkedModelSerializerWithPkField',
 
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
