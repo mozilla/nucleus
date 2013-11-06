027dfeda0998d6fef7f92999ae903e1f2817ff89
Michael Kelly <mkelly@mozilla.com>
2013-11-05 21:41:29 -0500

    Organize settings file, remove examples app, add temporary index.

--- nucleus/urls.py
+++ nucleus/urls.py
@@ -1,24 +1,25 @@
 from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
-from .examples import urls
+from django.http import HttpResponse
 
 from funfactory.monkeypatches import patch
-patch()
 
-from django.contrib import admin
-admin.autodiscover()
+
+patch()  # Apply funfactory monkeypatches.
+admin.autodiscover()  # Discover admin.py files for the admin interface.
+
 
 urlpatterns = patterns('',
     # Example:
-    (r'', include(urls)),
-    
+    (r'', include('nucleus.base.urls')),
+
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api-token-auth/', 'rest_framework.authtoken.views.obtain_auth_token'),
     url(r'^rna/', include('rna.urls')),
 
-    (r'^robots\.txt$', 
+    (r'^robots\.txt$',
         lambda r: HttpResponse(
             "User-agent: *\n%s: /" % 'Allow' if settings.ENGAGE_ROBOTS else 'Disallow' ,
             mimetype="text/plain"
