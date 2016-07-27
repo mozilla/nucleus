a1d876c0b7e821ab172d5e4b66382ba9ee5d2e42
Paul McLanahan <pmac@mozilla.com>
2016-07-26 21:59:48 -0400

    Remove home page test
    Home page is gone

--- nucleus/base/tests.py
+++ nucleus/base/tests.py
@@ -1,16 +1,7 @@
-from django.core.urlresolvers import reverse
 from django.test import TestCase
 
 
-class HomeTests(TestCase):
-
-    def test_base(self):
-        response = self.client.get(reverse('base.home'))
-        assert b'csrfmiddlewaretoken' in response.content
-
-
 class TestContribute(TestCase):
-
     def test_contribute_json(self):
         response = self.client.get('/contribute.json')
         self.assertEqual(response.status_code, 200)
