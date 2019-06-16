33c49207459ab56590b997c7e7247018e081fad6
Josh Mize <jgmize@gmail.com>
2019-06-16 11:11:42 -0500

    Update bin/update-config.sh sed regex to fix #105

--- bin/update-config.sh
+++ bin/update-config.sh
@@ -2,13 +2,13 @@
 set -ex
 # env vars: CLUSTER_NAME, CONFIG_BRANCH, CONFIG_REPO, NAMESPACE, DEPLOYMENT_YAML
 
-. ${BASH_SOURCE%/*}/../docker/bin/set_git_env_vars.sh # sets DOCKER_IMAGE_TAG
+. ${BASH_SOURCE%/*}/../docker/bin/set_git_env_vars.sh # sets DOCKER_IMAGE_TAG & DOCKER_REPOSITORY
 pushd $(mktemp -d)
 git clone --depth=1 -b ${CONFIG_BRANCH:=master} ${CONFIG_REPO:=github-mozmar-robot:mozmeao/nucleus-config} nucleus-config
 cd nucleus-config
 
 set -u
-sed -i -e "s|image: .*|image: ${DOCKER_IMAGE_TAG}|" ${CLUSTER_NAME:=iowa-b}/${NAMESPACE:=nucleus-dev}/${DEPLOYMENT_YAML:=deploy.yaml}
+sed -i -e "s|image: ${DOCKER_REPOSITORY}.*|image: ${DOCKER_IMAGE_TAG}|" ${CLUSTER_NAME:=iowa-b}/${NAMESPACE:=nucleus-dev}/${DEPLOYMENT_YAML:=deploy.yaml}
 git add ${CLUSTER_NAME}/${NAMESPACE}/${DEPLOYMENT_YAML}
 git commit -m "set image to ${DOCKER_IMAGE_TAG} in ${CLUSTER_NAME}" || echo "nothing new to commit"
 git push
