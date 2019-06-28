e5554cce78745ef180d98f6a9e606fcba3ef1c36
Paul McLanahan <pmac@mozilla.com>
2019-06-28 12:45:27 -0400

    Update Docker setup with multi-stage build
    And use docker-compose for CI and local dev

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -ex
 
 flake8 nucleus
+urlwait
 bin/post-deploy.sh
 python manage.py test
--- docker/bin/push2dockerhub.sh
+++ docker/bin/push2dockerhub.sh
@@ -9,8 +9,13 @@ source $BIN_DIR/set_git_env_vars.sh
 docker push $DOCKER_IMAGE_TAG
 
 if [[ "$GIT_TAG_DATE_BASED" == true ]]; then
-    docker tag $DOCKER_IMAGE_TAG $DOCKER_REPOSITORY:$GIT_TAG
-    docker push $DOCKER_REPOSITORY:$GIT_TAG
-    docker tag $DOCKER_IMAGE_TAG $DOCKER_REPOSITORY:latest
-    docker push $DOCKER_REPOSITORY:latest
+    # git tag
+    docker tag "$DOCKER_IMAGE_TAG" "$DOCKER_REPOSITORY:$GIT_TAG"
+    docker push "$DOCKER_REPOSITORY:$GIT_TAG"
+    # latest
+    docker tag "$DOCKER_IMAGE_TAG" "$DOCKER_REPOSITORY:latest"
+    docker push "$DOCKER_REPOSITORY:latest"
+    # builder latest for cache
+    docker tag "$DOCKER_REPOSITORY:builder-$GIT_TAG" "$DOCKER_REPOSITORY:builder-latest"
+    docker push "$DOCKER_REPOSITORY:builder-latest"
 fi
