7c1bcb68c16542debae2b05328e1843ab4c5c825
Rob Hudson <robhudson@mozilla.com>
2022-02-04 09:10:54 -0800

    Bump pip and pip-tools

--- bin/compile-requirements.sh
+++ bin/compile-requirements.sh
@@ -6,8 +6,8 @@ export CUSTOM_COMPILE_COMMAND="make compile-requirements"
 
 # We need this installed, but we don't want it to live in the main requirements
 # We will need to periodically review this pinning
-pip install --upgrade "pip<22"  # Temporary until pip-tools is fixed.
-pip install --upgrade pip-tools==6.4.0  # Needs at least this version to build.
+pip install --upgrade pip
+pip install --upgrade pip-tools==6.5.0  # Needs at least this version to build.
 pip install --upgrade pip-compile-multi
 
 pip-compile-multi \
