b020e47ecc0a938a68aa80a08cd2f03238fc7a26
Rob Hudson <robhudson@mozilla.com>
2022-03-30 17:14:32 -0700

    Settings updates
    The template context processors is required for the admin to enable the sidebar.

--- nucleus/settings.py
+++ nucleus/settings.py
@@ -127,6 +127,8 @@ WATCHMAN_CHECKS = (
 
 DATABASES = {"default": config("DATABASE_URL", cast=dj_database_url.parse)}
 
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
+
 CACHES = {
     "default": config("CACHE_URL", default="locmem://", cast=django_cache_url.parse),
 }
@@ -187,6 +189,7 @@ TEMPLATES = [
                 "django.template.context_processors.debug",
                 "django.template.context_processors.i18n",
                 "django.template.context_processors.media",
+                "django.template.context_processors.request",
                 "django.template.context_processors.static",
                 "django.template.context_processors.tz",
                 "django.contrib.messages.context_processors.messages",
