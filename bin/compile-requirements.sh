0f3ae9305bc7baff926ef219f91c0cd7570e92f2
Steve Jalim <stevejalim@mozilla.com>
2024-01-08 11:05:47 +0000

    Purge requirements.txt files so subdeps will get automatically updated

--- bin/compile-requirements.sh
+++ bin/compile-requirements.sh
@@ -4,6 +4,9 @@ set -exo pipefail
 
 export CUSTOM_COMPILE_COMMAND="make compile-requirements"
 
+# Purge old requirements/*.txt files so we get our subdeps automatically upgraded if allowed
+rm -f requirements/*.txt
+
 # We need this installed, but we don't want it to live in the main requirements
 # We will need to periodically review this pinning
 pip install --upgrade pip
