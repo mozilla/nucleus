9bf66275b532b7ba464cf8b607574b1db1c6aec7
Steve Jalim <stevejalim@mozilla.com>
2024-07-08 14:48:42 +0100

    Switch to uv for dependency compilation

--- bin/compile-requirements.sh
+++ bin/compile-requirements.sh
@@ -1,19 +1,19 @@
-#!/bin/bash
+#! /bin/bash
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 set -exo pipefail
 
-export CUSTOM_COMPILE_COMMAND="make compile-requirements"
-
-# Purge old requirements/*.txt files so we get our subdeps automatically upgraded if allowed
-rm -f requirements/*.txt
+# Set the command used in the reminder comment at the top of the file
+export UV_CUSTOM_COMPILE_COMMAND="$ make compile-requirements"
 
 # We need this installed, but we don't want it to live in the main requirements
-# We will need to periodically review this pinning
-pip install --upgrade pip
-pip install --upgrade pip-tools
-pip install --upgrade pip-compile-multi
+pip install -U uv
 
-pip-compile-multi \
-  --generate-hashes prod \
-  --generate-hashes dev \
-  --header=/app/requirements/.pip-compile-multi-header.txt
+# Drop the compiled reqs files, to help us pick up automatic subdep updates, too
+rm -f requirements/*.txt
+
+uv pip compile --generate-hashes --no-strip-extras requirements/prod.in -o requirements/prod.txt
+uv pip compile --generate-hashes --no-strip-extras requirements/dev.in -o requirements/dev.txt
